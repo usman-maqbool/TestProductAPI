e6e2c5ab38380f4d852845c6c3890247679287a8
usman_maqbool <engusman8279@gmail.com>
2023-09-17 20:37:32 +0500

    added media dir and conf

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import include, path
 from products_app.views import ProductViewSet
+from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet)
@@ -11,3 +13,4 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
